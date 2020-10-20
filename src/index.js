c0208bd1cd9debc161e314a5d000e4b0630b5abb
elygo <el.uzb93@gmail.com>
2020-10-21 01:54:47 +0500

    init: add func balanced brackets
    The function for checking if brackets are balanced is implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const conf = Object.fromEntries(bracketsConfig);
+  var stack = [];
+
+  for(var i = 0; i < str.length; i++){
+  		switch(str.charAt(i)){
+  			case '(': case '[': case '{': case '1': case '3': case '5':
+  				stack.push(str.charAt(i));
+  			break;
+  			case ')': case ']': case '}': case '2': case '4': case '6':
+  				if(conf[stack.pop()] !== str.charAt(i)) return false;
+  			break;
+  			case '|': case '7': case '8':
+  				if(stack.length === 0 || stack[stack.length-1] !== str.charAt(i)){
+  					stack.push(str.charAt(i));
+  				} else {
+  					stack.pop();
+  				}
+  		}
+  	}
+
+
+  return stack.length === 0;
+
 }
